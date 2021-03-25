c5bf74dc341e26382cd01e9a7906565ee1534e4d
ValentynKurylo <56786816+ValentynKurylo@users.noreply.github.com>
2021-03-25 12:48:46 +0200

    task_5.html task_5.js

---
+++ task_5/task_5.js
@@ -0,0 +1,6 @@
+let num_1 = prompt("Enter number");
+let num_2 = prompt("Enter number");
+let num_3 = prompt("Enter number");
+let num = num_1 +num_2 + num_3;
+num = Number.parseInt(num, 10);
+console.log(num);
