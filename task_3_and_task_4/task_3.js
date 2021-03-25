8d494f0802f1a4c08b968e0274533eb0bcaf08f3
ValentynKurylo <56786816+ValentynKurylo@users.noreply.github.com>
2021-03-25 12:42:33 +0200

    task_3.html task_3.js

---
+++ task_3_and_task_4/task_3.js
@@ -0,0 +1,6 @@
+let name = prompt("Enter your name");
+let surname = prompt("Enter your surname");
+let age = prompt("Enter your age");
+alert("Вітаю " + name + " " + surname + ". Тобі " + age + " років.");
+let information = "Вітаю " + name + " " + surname + ". Тобі " + age + " років.";
+document.write(information);
